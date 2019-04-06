7629e1ab217da2ac6ba83d5f8b22cece1d05d570
Sumanth <manchala.msr@gmail.com>
2019-04-06 09:29:14 +0530

    Improve file search and added a vector to store ip

--- datagram.java
+++ datagram.java
@@ -69,8 +69,8 @@ class FileSearch
         } 
  
     }*/
-    public Boolean findFile(String name,File file)
-    {
+    public void findFile(String name,File file)
+    { 
         File[] list = file.listFiles();
         if(list!=null)
         for (File fil : list)
@@ -81,20 +81,20 @@ class FileSearch
             }
             else if (name.equalsIgnoreCase(fil.getName()))
             {
-                //System.out.println(fil.getParentFile());
-                //this.files.add(fil.getParentFile());
-                return true;
+                receive.pf = fil.getParentFile();
             }
         }
-        return false;
     }
 }
 class receive extends Thread
 {
     DatagramSocket dss;
+    static File pf;
+
     receive() throws SocketException
     {
         dss = new DatagramSocket(3333);
+        pf = null;
     }
 
     @Override
@@ -114,12 +114,14 @@ class receive extends Thread
                     String msg = new String(dp.getData(),0,dp.getLength());
                     if(!(msg.equalsIgnoreCase("file found")||msg.equalsIgnoreCase("file not found")))
                     {
+                        pf = null;
                         System.out.println("Requested file name is : "+dp.getAddress().getHostAddress()+" "+msg);
                         FileSearch fs = new FileSearch();
                         String respone;
-                        if(fs.findFile(msg,new File("/home/sumanth/Documents")))
+                        fs.findFile(msg,new File("/home/sumanth/Documents"));
+                        if(pf!=null)
                         {
-                            respone = "File found";
+                            respone = "File found at " + pf.getName();
                         }
                         else
                         {
@@ -129,10 +131,13 @@ class receive extends Thread
 
                     }
                     else
-                    {
-                        System.out.println(msg);
+                    {  
+                       System.out.println(msg);
+                       if(msg.startsWith("File found"))
+                       {
+                           send.ips.add(dp.getAddress().getHostAddress());
+                       }
                     }
-                   
                     
                     //dss.send(new Datagra);
                 }
@@ -150,6 +155,7 @@ class send extends Thread
 {
     Scanner sc;
     DatagramSocket ds;
+    static Vector ips = new Vector<String>();
     send() throws Exception
     {
         ds = new DatagramSocket();
@@ -174,7 +180,10 @@ class send extends Thread
             try
             {
                 DatagramPacket dp = new DatagramPacket(msg.getBytes(),msg.length(),InetAddress.getByName("172.30.105.255"),3333);
+                ips.clear();
                 ds.send(dp);
+                Thread.sleep(4000);
+                System.out.println("InetAddresses" +ips);
                // ds.close();
             }
             catch(Exception e)
